8ae71a67ab4ea0ef9b4a7e5fbb979585977360d6
VigilantBag <vigilantbag2018@gmail.com>
2022-11-15 18:36:09 -0800

    Added custom api url

--- osmCheckNearbyForData.py
+++ osmCheckNearbyForData.py
@@ -1,7 +1,9 @@
 import overpy
-api = overpy.Overpass()
+import time
 
-radius = 100
+api = overpy.Overpass(url="https://7be2-209-129-115-56.ngrok.io/api/interpreter/") #Change url as needed, but keep subdirectory /api/interpreter/
+
+radius = 100 #meters
 lat = 34.167282045937164
 long = -119.03844158095751
 
@@ -13,7 +15,9 @@ while(natural == "null"):
         way = result.ways[1]
         natural = way.tags["natural"]
     except(IndexError):
-        radius = radius+100
+        radius = radius+10
         print("new radius = {}".format(radius))
+    except(overpy.exception.OverpassTooManyRequests):
+        time.sleep(1000)
     
 print(natural)
