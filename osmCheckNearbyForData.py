e0bea52b1d8717806319cb6b1f79c4a111d8b1ee
VigilantBag <vigilantbag2018@gmail.com>
2022-11-22 11:29:30 -0800

    multiple tags

--- osmCheckNearbyForData.py
+++ osmCheckNearbyForData.py
@@ -3,7 +3,7 @@ import time
 import Namedtuple
 from collections import namedtuple
 
-api = overpy.Overpass(url="https://fc77-209-129-115-56.ngrok.io/api/interpreter/", max_retry_count=20) #Change url as needed, but keep subdirectory /api/interpreter/
+api = overpy.Overpass(url="https://4d0e-209-129-115-55.ngrok.io/api/interpreter/", max_retry_count=20) #Change url as needed, but keep subdirectory /api/interpreter/
 
 radius = 100 #meters
 lat = 34.167282045937164
@@ -12,29 +12,36 @@ long = -119.03844158095751
 ways = []
 
 natural = "null"
-
+landuse = "null"
 searchtype = "natural"
 
 wayData = namedtuple("wayData", ["natural", "landuse", "isBuilding"])
 
-i = 0
+i = 1
 
-while(i <= 20):
-    result = api.query('[out:json];way[natural](around:{},{},{}); out;'.format(radius, lat, long))
+while(i <= 2):
+    result = api.query('[out:json];way["natural"](around:{0},{1},{2});way["landuse"](around:{0},{1},{2}); out;'.format(radius, lat, long))
     try:
-        way = result.ways[i]
+        way = result.ways[0]
         #print(way.tags)
-        natural = way.tags["natural"]
-        ways[i] = Namedtuple.gridValues(way.tags["landcover"], way.tags["natural"], way.tags["landuse"], null, way.tags["*road*"], way.tags["building"], null, null )
-        print(ways[i])
-        i = i+1
+        try:
+            natural = way.tags["natural"]
+            ways[i] = Namedtuple.gridValues("null", way.tags["natural"], "null", "null", "null", "null", "null", "null")
+        except(KeyError):
+            try:
+                landuse = way.tags["landuse"]
+                ways[i] = Namedtuple.gridValues("null", "null", way.tags["landuse"], "null", "null", "null", "null", "null")
+            except(KeyError):
+                print(ways[i])
+                i = i+1
     except(KeyError):
         radius = radius+100
         print("new radius = {}".format(radius))
         continue
     except(IndexError):
         radius = radius+100
-        print("new radius = {}".format(radius))
+        print("INDEX new radius = {}".format(radius))
 
     
 print(natural)
+print(landuse)
