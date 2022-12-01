a1717986e64e86c27450417f9f4d3d16d9bd1b46
VigilantBag <vigilantbag2018@gmail.com>
2022-12-01 15:21:28 -0800

    fill coordinateGrid with latlon pairs

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ lonFt = startLon*3280.4
 
 #Fill coordinateGrid with 100ft away from start location
 coordinateGrid[0][0] = (latFt, lonFt)
+lat = 0
+lon = 0
+inc = 0
+while coordinateGrid[4][4] == 16:
+    while lat < 5:
+        coordinateGrid[lat][lon] = (latFt+inc, lonFt)
+        lat = lat+1
+        inc = inc+100
+    lat = 0
+    while lon < 5:
+        coordinateGrid[lat][lon] = (latFt, lonFt+inc)
+        lon = lon+1
+        inc = inc+100
+    lon = 0
+        
