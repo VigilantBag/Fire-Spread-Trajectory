ed8749858059dd44e55bab3374cbab845d05c821
VigilantBag <vigilantbag2018@gmail.com>
2022-12-03 09:55:53 -0800

    still looping

--- main.py
+++ main.py
@@ -13,35 +13,30 @@ lon = 0
 inc = 0
 latCounter = 0
 lonCounter = 0 
+loopcounter = 0
 while coordinateGrid[3][3] == 16:
     wlat = lat
     while latCounter < 5:
         coordinateGrid[lat][lon] = (latFt+inc, lonFt)
         coordinatePairs = coordinateGrid[lat][lon]
-        (latitude, longitude) = coordinatePairs
+        latitude, longitude = coordinatePairs
         latfttoCoord = latitude/3280.34/111.1111111111111
         lonfttoCoord = longitude/3280.34/111.1111111111111
         osm(latfttoCoord, lonfttoCoord)
         wlat = wlat+1
-        coordinateGrid[lat][lon] = (latFt, lonFt-inc)
-        coordinatePairs = coordinateGrid[lat][lon]
-        (latitude, longitude) = coordinatePairs
-        latfttoCoord = latitude/3280.34/(10,000/90)
-        lonfttoCoord = longitude/3280.34/(10,000/90)
-        osm(latfttoCoord, lonfttoCoord)
-        lon = lon+1
-        inc = inc+100
         latCounter += 1
-        if latCounter = 5:
+        if latCounter == 5:
             lat = 0
             latCounter = 0
+        loopcounter = loopcounter+1
+        print(loopcounter)
 
     lat = 0
     #infinite loop as lat and lon keep being set to 0 everytime while runs again. Need to fix
     while lonCounter < 5:
         coordinateGrid[lat][lon] = (latFt, lonFt-inc)
         coordinatePairs = coordinateGrid[lat][lon]
-        (latitude, longitude) = coordinatePairs
+        latitude, longitude = coordinatePairs
         latfttoCoord = latitude/3280.34/(10,000/90)
         lonfttoCoord = longitude/3280.34/(10,000/90)
         osm(latfttoCoord, lonfttoCoord)
@@ -51,7 +46,8 @@ while coordinateGrid[3][3] == 16:
         if lonCounter == 5:
             lon = 0
             lonCounter = 0
-        
-    
+        loopcounter = loopcounter+1
+        print(loopcounter)
+print(coordinateGrid)
 
         
