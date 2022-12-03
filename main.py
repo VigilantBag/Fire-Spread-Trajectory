737188457920b4a6a487334513259b5edff5cfc6
VigilantBag <vigilantbag2018@gmail.com>
2022-12-03 10:01:43 -0800

    stop gap fix for infini loop

--- main.py
+++ main.py
@@ -12,31 +12,50 @@ lat = 0
 lon = 0
 inc = 0
 counter = 0
-while coordinateGrid[3][3] == 16:
-    wlat = lat
-    while lat < 5:
-        coordinateGrid[lat][lon] = (latFt+inc, lonFt)
-        coordinatePairs = coordinateGrid[lat][lon]
-        (latitude, longitude) = coordinatePairs
-        latfttoCoord = latitude/3280.34/111.1111111111111
-        lonfttoCoord = longitude/3280.34/111.1111111111111
-        osm(latfttoCoord, lonfttoCoord)
-        wlat = wlat+1
-        coordinateGrid[lat][lon] = (latFt, lonFt-inc)
-        coordinatePairs = coordinateGrid[lat][lon]
-        (latitude, longitude) = coordinatePairs
-        latfttoCoord = (latitude,)/3280.34/(10,000/90)
-        lonfttoCoord = (longitude)/3280.34/(10,000/90)
-        osm(latfttoCoord, lonfttoCoord)
-        lon = lon+1
-        inc = inc+100
+while counter <= 3:
+    coordinateGrid[0][lon] = (latFt, lonFt-inc)
+    coordinatePairs = coordinateGrid[lat][lon]
+    latitude, longitude = coordinatePairs
+    latfttoCoord = latitude/3280.34/(10,000/90)
+    lonfttoCoord = longitude/3280.34/(10,000/90)
+    osm(latfttoCoord, lonfttoCoord)
+    lon = lon+1
+    inc = inc+100
+while counter <= 3:
+    coordinateGrid[1][lon] = (latFt, lonFt-inc)
+    coordinatePairs = coordinateGrid[lat][lon]
+    latitude, longitude = coordinatePairs
+    latfttoCoord = latitude/3280.34/(10,000/90)
+    lonfttoCoord = longitude/3280.34/(10,000/90)
+    osm(latfttoCoord, lonfttoCoord)
+    lon = lon+1
+    inc = inc+100
+while counter <= 3:
+    coordinateGrid[2][lon] = (latFt, lonFt-inc)
+    coordinatePairs = coordinateGrid[lat][lon]
+    latitude, longitude = coordinatePairs
+    latfttoCoord = latitude/3280.34/(10,000/90)
+    lonfttoCoord = longitude/3280.34/(10,000/90)
+    osm(latfttoCoord, lonfttoCoord)
+    lon = lon+1
+    inc = inc+100
+while counter <= 3:
+    coordinateGrid[3][lon] = (latFt, lonFt-inc)
+    coordinatePairs = coordinateGrid[lat][lon]
+    latitude, longitude = coordinatePairs
+    latfttoCoord = latitude/3280.34/(10,000/90)
+    lonfttoCoord = longitude/3280.34/(10,000/90)
+    osm(latfttoCoord, lonfttoCoord)
+    lon = lon+1
+    inc = inc+100
+    
 
     lat = 0
     #infinite loop as lat and lon keep being set to 0 everytime while runs again. Need to fix
     """while lon < 5:
         coordinateGrid[lat][lon] = (latFt, lonFt-inc)
         coordinatePairs = coordinateGrid[lat][lon]
-        (latitude, longitude) = coordinatePairs
+        latitude, longitude = coordinatePairs
         latfttoCoord = latitude/3280.34/(10,000/90)
         lonfttoCoord = longitude/3280.34/(10,000/90)
         osm(latfttoCoord, lonfttoCoord)
