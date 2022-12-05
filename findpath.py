73be98b77e229dfece227d58c0bf852b08330cc6
Demonkingasta710 <115735952+Demonkingasta710@users.noreply.github.com>
2022-12-05 09:19:54 -0800

    implemented into function call

--- findpath.py
+++ findpath.py
@@ -3,13 +3,15 @@ from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from grid import weightedGrid
 
-grid = Grid(weightedGrid = weightedGrid)
+def findPath():
+  matrix = weightedGrid
+  grid = Grid(matrix=matrix)
 
-start = grid.node(0, 0)
-end = grid.node(2, 2)
+  start = grid.node(0, 0)
+  end = grid.node(3, 2)
 
-finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-path, runs = finder.find_path(start, end, grid)
+  finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+  path, runs = finder.find_path(start, end, grid)
 
-print('operations:', runs, 'path length:', len(path))
-print(grid.grid_str(path=path, start=start, end=end))
+  print('operations:', runs, 'path length:', len(path))
+  print(grid.grid_str(path=path, start=start, end=end))
